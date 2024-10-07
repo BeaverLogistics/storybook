f215e558fdf556ed5445b291b800cdc1b665119f
BeaverLogistics <coderun39824@gmail.com>
2024-10-08 01:21:23 +1100

    Updates

---
+++ book.js
@@ -0,0 +1,18 @@
+window.BOOK = {
+  title: "Max's Perfect Pack",
+  cover: {
+    title: "Max's Perfect Pack",
+    subtitle: "",
+    author: 'Thang Nguyen',
+    illustrator: 'Stable Diffusion',
+  },
+  pages: [
+    { images: ['0.png'], textPosition: 'top', text: `Hi, my name is Thang and I am a Solution Design Engineer at CEVA Logistics. I wanted to challenge myself to create something a bit different and using technologies I haven't used before. This was made in less than 48 hours without any prior knowledge in web development. <br></br>I love learning but most of all, I love working with others to achieve a common goal. It is very exciting to be able to meet others at Tangram and build invaluable relationships. If possible, it would be greatly beneficial to work on a space utilisation optimiser. This will reduce waste dramatically and is replicable across the business lines. <br></br>This quick story is just a quick explanation to a child why space utilisation is very important. Please enjoy!` },
+    { images: ['1.png'], textPosition: 'bottom', text: `In a sunny little town, there lived a boy named Max. Max loved adventures but always forgot things because he never packed his bag properly!` },
+    { images: ['2.png'], textPosition: 'bottom', text: `Every time he got ready, his room looked like a tornado had hit! “Oh no!” Max exclaimed. “I forgot my favourite toy again!”` },
+    { images: ['3.png'], textPosition: 'bottom', text: `One day, Max's mum said, “If you pack neatly, you won’t forget anything!” Max thought about it. “I want to have a great adventure!” he declared.` },
+    { images: ['4.png'], textPosition: 'bottom', text: `Max gathered his toys, clothes, and snacks. One by one, he packed them neatly in his bag. “This is much easier!” he giggled.` },
+    { images: ['5.png'], textPosition: 'bottom', text: `With everything packed, Max played outside with his friends. He realised he didn’t need to go back home for anything. “Packing neatly is the best!” he said.` },
+    { images: ['6.png'], textPosition: 'bottom', text: `From that day on, Max always packed neatly. He learned that a tidy bag leads to a happy day!` },
+  ]
+}
